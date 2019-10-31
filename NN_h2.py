51bbdfb3ac96a6f16b787d7363b2308f02d419ce
ruint03 <qlsbinu@yonsei.ac.kr>
2019-10-31 10:14:31 +0900

    Update NN_h2.py

--- NN_h2.py
+++ NN_h2.py
@@ -1,6 +1,8 @@
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import numpy as np
 from itertools import chain
+
+tf.disable_v2_behavior()
 
 fp = open("train.txt", "r", encoding="utf-8")
 a_x = list()
@@ -37,15 +39,15 @@ X = tf.placeholder(tf.float32, [None, 784])
 Y = tf.placeholder(tf.float32, [None, 10])
 keep_prob = tf.placeholder(tf.float32)
 
-W1 = tf.Variable(tf.random_nomal([784, 256], stddev=0.01))
+W1 = tf.Variable(tf.random_normal([784, 256], stddev=0.01))
 L1 = tf.nn.relu(tf.matmul(X, W1))
 L1 = tf.nn.dropout(L1, keep_prob)
 
-W2 = tf.Variable(tf.random_nomal([256, 256], stddev=0.01))
+W2 = tf.Variable(tf.random_normal([256, 256], stddev=0.01))
 L2 = tf.nn.relu(tf.matmul(L1, W2))
 L2 = tf.nn.dropout(L2, keep_prob)
 
-W3 = tf.Variable(tf.random_nomal([256, 10], stddev=0.01))
+W3 = tf.Variable(tf.random_normal([256, 10], stddev=0.01))
 model = tf.matmul(L2, W3)
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=model))
@@ -77,7 +79,7 @@ for epoch in range(60):
     print('Epoch:', '%04d' % (epoch + 1), 'Avarage cost=', total_cost / (total_batch))
 print('Finishing Training.')
 ######### Test the performance of the system ###########################################
-fp = open("test.txxt", "r", encoding="utf-8")
+fp = open("test.txt", "r", encoding="utf-8")
 test_all_x = list()
 test_all_z = list()
 
